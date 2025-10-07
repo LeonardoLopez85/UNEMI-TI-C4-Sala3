fc243cd73efaee383b559101f59180f2a5552f5e
Leonardo <leo_lalmhotmail.com>
2025-10-06 20:13:06 -0500

    Mi primer commit de los archivos

---
+++ UNEMI_ITI_FP_Grupo11.py
@@ -0,0 +1,15 @@
+# Programa para identificar si un número es positivo, negativo o cero
+ 
+# Entrada
+numero = int(input("Introduce un número: "))
+ 
+# Proceso y salida
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
+
+# Esta línea mantendrá la ventana abierta
+input("Presiona Enter para salir...")
