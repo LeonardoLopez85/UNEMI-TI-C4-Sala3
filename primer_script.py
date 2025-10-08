14ad3478194299a0467954303b5cad543ccbc540
Leonardo López Martínez <llopezm21@unemi.edu.ec>
2025-10-08 16:49:30 -0500

    Mi primer commit de los archivos

--- primer_script.py
+++ primer_script.py
@@ -1,8 +1,10 @@
-# Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-# Enter "help" below or click "Help" above for more information.
+# En equipo, creen un archivo llamado primer_script.py.
+# Escriban el siguiente código adaptado con la información de su grupo:
 print("Nombre del grupo: Sala 3")
 print("Integrantes: [Lista de nombres completos]")
 print("Carrera o especialidad del grupo: Tecnologias de la Informacion ")
 print("Estamos aprendiendo Python")
+# Ejecuten el archivo desde IDLE o desde la terminal:
+# python3 primer_script.py
 # Esta línea mantendrá la ventana abierta
 input("Presiona Enter para salir....")
