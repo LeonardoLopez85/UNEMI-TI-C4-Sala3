99ad890785fde3d45bac3e7bbe8225179684c1c6
dcriolloc2 <dcriolloc2@unemi.edu.ec>
2025-10-08 21:55:40 -0500

    Update primer_script.py
    Descripción de los integrantes.

--- primer_script.py
+++ primer_script.py
@@ -1,7 +1,7 @@
-# En equipo, creen un archivo llamado primer_script.py.
+er# En equipo, creen un archivo llamado primer_script.py.
 # Escriban el siguiente código adaptado con la información de su grupo:
-print("Nombre del grupo: Sala 3")
-print("Integrantes: [Lista de nombres completos]")
+print("Nombre del grupo: Pre Feriado")
+print("Integrantes: [JOSELYN JACINTA GILER ALAVA, IRENE ROSALIA AYALA TRIVIÑO, DAYRA ESTEFANIA PLUA DESIDERIO, Gilda Ronquillo, FRANKLIN JUNIOR VINCES CHAMBA, LEONARDO ALBERTO LOPEZ MARTINEZ, RENE PATRICIO ROBALINO VITERI, ILIANA ELIZABETH PAREDES ZAVALA, ERICK ROMARIO BORJA PASTRANA, INGRID LIZBETH DE LA CRUZ DELGADO, SELENA THALIA MUÑOZ BARRAGAN, JAHIR ALEJANDRO LOZANO PALMA, GERALDO SEBASTIAN LOPEZ ANTEPARA, SELENA THALIA MUÑOZ BARRAGAN, VICTOR JOSUE DIAZ ALCIVAR, DANIELA SHEILOT MAGALLANES HOLGUIN, LEONARDO LOPEZ, DARWIN JAVIER CRIOLLO CRIOLLO]")
 print("Carrera o especialidad del grupo: Tecnologias de la Informacion ")
 print("Estamos aprendiendo Python")
 # Ejecuten el archivo desde IDLE o desde la terminal:
